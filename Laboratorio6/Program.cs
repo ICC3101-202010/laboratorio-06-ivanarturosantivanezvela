70bf6295c9053b8dea591154fc78e0393eddaafe
Ivan Arturo Santivañez Vela <Ivan@MacBook-Pro-de-Clementina-757.local>
2020-05-19 13:47:11 -0400

    final

--- Laboratorio6/Program.cs
+++ Laboratorio6/Program.cs
@@ -15,7 +15,9 @@ namespace Laboratorio6
             while (!value)
             {
                 Console.WriteLine("1- Si desea utilizar un archivo para cargar la informacion de su empresa 2- Ingresar datos manuales de mi empresa  3-Salir");
-                string opcion =Console.ReadLine();
+                string opcion = Console.ReadLine();
+
+               
                 
                 switch (opcion)
                 {
@@ -29,9 +31,55 @@ namespace Laboratorio6
 
                         }
                         catch(Exception)
-                        {
+                        {   
                             Console.WriteLine("No se encontro la empresa, se solicitara de forma manual");
-                            opcion = "2";
+                            Console.WriteLine("Escriba el nombre de la empresa");
+                            string nombre1 = Console.ReadLine();
+                            Console.WriteLine("Escriba el rut de la empresa");
+                            int rut1 = Convert.ToInt32(Console.ReadLine());
+                            Empresa empresa1 = new Empresa(nombre1, rut1);
+
+                            Area area1 = new Area("Economia");
+                            area1.Personaencargada = new Persona("Juan", "Perez", 193242006, "Gerente");
+                            area1.Listadepersonas.Add(new Persona("Ivan", "Santivanez", 19078504, "Consejero"));
+
+
+
+                            Departamento departamento1 = new Departamento("Cuentas nacionales");
+                            departamento1.Personaencargada = new Persona("Diana", "Lopez", 14572216, "Jefe");
+                            departamento1.Listadepersonas.Add(new Persona("Pamela", "Vela", 14485792, "Senior"));
+
+                            area1.Departamentos.Add(departamento1);
+
+                            Seccion seccion1 = new Seccion("Gobierno");
+                            seccion1.Personaencargada = new Persona("Agustin", "Fernandez", 19322466, "Jefe");
+                            seccion1.Listadepersonas.Add(new Persona("James", "Mcgil", 14078905, "Coordinador"));
+
+                            departamento1.Secciones.Add(seccion1);
+
+                            Bloque bloque11 = new Bloque("Gobierno municipal");
+                            bloque11.Personaencargada = new Persona("Diego", "Aviles", 145728, "Ayudante");
+                            bloque11.Listadepersonas.Add(new Persona("Saul", "Godmas", 1234565, "Digitador"));
+
+                            Bloque bloque22 = new Bloque("Gobierno general");
+                            bloque22.Personaencargada = new Persona("Javier", "Gonzalez", 1324458, "Ayudante");
+                            bloque22.Listadepersonas.Add(new Persona("Kim", "Waxler", 1234565, "Digitador"));
+
+                            seccion1.Bloques.Add(bloque11);
+                            seccion1.Bloques.Add(bloque22);
+
+                            empresa1.Divisiones.Add(area1);
+                            empresa1.Divisiones.Add(departamento1);
+                            empresa1.Divisiones.Add(seccion1);
+                            empresa1.Divisiones.Add(bloque11);
+                            empresa1.Divisiones.Add(bloque22);
+
+                            Empresa.Serializar("empresa.bin", empresa1);
+                            empresa1.Empresainformacion();
+
+
+
+
 
                         }
                        
@@ -92,6 +140,7 @@ namespace Laboratorio6
 
 
                 }
+
                
 
             }
